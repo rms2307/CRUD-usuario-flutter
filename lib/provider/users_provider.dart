5e56d22ba79f1196b76195da9f5befd7ec76839d
Robson Silva <rms2307@outlook.com>
2020-07-28 16:42:38 -0300

    metodo delete

--- lib/provider/users_provider.dart
+++ lib/provider/users_provider.dart
@@ -48,4 +48,11 @@ class UsersProvider with ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void remove(User user) {
+    if (user != null && user.id != null) {
+      _items.remove(user.id);
+      notifyListeners();
+    }
+  }
 }
