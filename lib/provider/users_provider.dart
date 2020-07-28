21c036085d5210391b1d1f5c561e63a2d50f0fd5
Robson Silva <rms2307@outlook.com>
2020-07-28 16:37:31 -0300

    metodo de insert/update

--- lib/provider/users_provider.dart
+++ lib/provider/users_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:CRUD_flutter/data/dummy_users.dart';
 import 'package:CRUD_flutter/models/users.dart';
 import 'package:flutter/cupertino.dart';
@@ -15,5 +17,35 @@ class UsersProvider with ChangeNotifier {
 
   User byIndex(int i) {
     return _items.values.elementAt(i);
+  }
+
+  void put(User user) {
+    if (user == null) {
+      return;
+    }
+
+    if (user.id != null &&
+        // Update
+        user.id.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
+      _items.update(
+          user.id,
+          (_) => User(
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              avatarUrl: user.avatarUrl));
+    } else {
+      // Insert
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+          id,
+          () => User(
+              id: id,
+              name: user.name,
+              email: user.email,
+              avatarUrl: user.avatarUrl));
+    }
+    notifyListeners();
   }
 }
